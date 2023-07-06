099317be03f1bc7d5f315d1b9ee1b0cafadb265e
Webamz <chriszxn99@gmail.com>
2023-07-06 14:42:02 +0200

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,8 @@
-# puppet code to install & configure 'nginx'
-package { 'nginx':
-  ensure => installed,
-}
-
-file { '/etc/nginx/sites-available/default':
-  content => "server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-
-                root /var/www/html;
-                index index.html index.htm index.nginx-debian.html;
-
-                server_name _;
-
-                add_header X-Served-By $hostname;
-
-                location / {
-                        try_files $uri $uri/ =404;
-                }
-        }",
-  mode    => '0644',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure     => 'running',
-  enable     => true,
-  subscribe  => File['/etc/nginx/sites-enabled/default'],
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
